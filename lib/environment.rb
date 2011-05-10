88d57aa114cec82c8d9ad77b271a2fc3934ea560
Inilus ZIO <inilus@gmail.com>
2011-05-10 16:56:55 +0400

    Check rake file.

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,9 +1,9 @@
 require 'rubygems'
-require 'active_record'   # http://snippets.dzone.com/posts/show/3097
+require 'active_record'   # http://snippets.dzone.com/posts/show/3097 # http://habrahabr.ru/blogs/ruby/98751/
 require 'yaml'
  
 # Загружаем файл настройки соединения с БД
-dbconfig = YAML::load(File.open( '../config/database.yml') )
+dbconfig = YAML::load( File.open( 'config/database.yml' ) )
  
 # Ошибки работы с БД направим в стандартный поток (консоль)
 #ActiveRecord::Base.logger = Logger.new(STDERR) # Simple logging utility. logger.rb -- standart lib
